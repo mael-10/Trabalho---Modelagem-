739a8c753b6c194f1e3799c565d2c3d01219947f
Ismael <maelprogrammer1@gmail.com>
2024-03-14 10:40:58 -0300

    Adicionado excluir agenda

--- php/agendar.php
+++ php/agendar.php
@@ -11,13 +11,17 @@
 
     $data = $_POST ['Data_consulta'];
     $motivo = $_POST ['Motivo_consulta'];
-    $data_formatada = date('Y-m-d', strtotime($_POST['Data_consulta']));
+    $data_formatada = date('Y-m-d', strtotime($data));
 
      
 
     $insere = mysqli_query($conexao,"INSERT INTO agendar_consulta(Data_consulta, Motivo_consulta)
     VALUES ('$data_formatada ','$motivo')")
     or die(mysqli_error());
+
+    
+    echo "Data recebida: $data";
+
 
     header("Location: ../html/menu.html?mensagem=" . urlencode($mensagem));
     exit();   
---
+++ php/excluir.agend.php
@@ -0,0 +1,32 @@
+<html>
+<head> 
+    <title>Excluir Agendamento</title>
+</head>
+<body>
+    <?php
+    // Estabelece conexão com o servidor e o BD
+    include_once('conexao.php');
+
+    // Verifica se os dados do formulário foram recebidos
+    if(isset($_POST['Cod_consulta'])) {
+        // Recebe o código da consulta a ser excluída
+        $data_consulta = $_POST['Data_consulta'];
+        $motivo_consulta = $_POST['Motivo_consulta'];
+        
+
+        // Tenta excluir a consulta do banco de dados
+        $exclui = mysqli_query($conexao, "DELETE FROM agendar_Consulta WHERE Motivo_consulta = '$motivo_consulta' AND Data_consulta = '$data_consulta'")
+                  or die(mysqli_error($conexao));
+        
+        // Verifica se a exclusão foi bem-sucedida
+        if($exclui) {
+            echo "Consulta excluída com sucesso!";
+        } else {
+            echo "Ocorreu um erro ao excluir a consulta.";
+        }
+    } else {
+        echo "Os dados do formulário não foram recebidos.";
+    }
+    ?>
+</body>
+</html>
