fd0ca4072af673bb3259100a22e5462651f7155b
Ismael <maelprogrammer1@gmail.com>
2024-03-17 17:45:43 -0300

    Atualizado

--- php/mostrar.php
+++ php/mostrar.php
@@ -1,21 +1,22 @@
 <html>
   <head>
      <title>Cliente</title>
-	 <link rel = "stylesheet" href = "../estilo/estilizacao.css">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	   <link rel = "stylesheet" href = "../style/style.css">
   </head>
-  <body class = "cad_tutor">
-  <div id = "principal">
+  <body class = "cad_tutor background-gradiente">
+  <div id = "principalf container">
    <main>
-
-	  <fieldset>
-	   <legend>Consultar Dados</legend>
+  <div class="d-flex justify-content-center my-3"> 
+    <div>
+	   <legend class="text-white">Consultar Dados</legend>
       <form id="buscar" action="mostrar2.php" method="post">
         <input type="date" id="data" name="data">
         <button type="submit">buscar</button>
+    </div>
+  </div>
       </form>
-
-	   <table border="1">
-
+        <table class="table container">
 			 <?php
 				// importa dados de conexão
 			    include_once('conexao.php');
@@ -23,27 +24,30 @@
 				// consulta os dados da tabela UF
 				
 				$consulta = mysqli_query($conexao, "SELECT  cod_consulta as cod, data_consulta as data_da_consulta, motivo_consulta as motivo FROM agendar_consulta ORDER BY data_consulta");
-  
-         echo "<tr>";
-         echo "<td>Código</td>";
-         echo "<td>data da consulta</td>";
-         echo "<td>motivo da consulta</td>";
-         echo "</tr>";
+        
+      
+        echo "<thead>";
+          echo "<tr>"; 
+            echo '<th scope="col">Cod Consulta</th>';
+            echo '<th scope="col">Data da Consulta</th>';
+            echo '<th scope="col">Motivo da Consulta</th>';
+          echo "</tr>"; 
+       echo "</thead>"; 
+      
 
-
-							
-				  while($resultado = mysqli_fetch_array($consulta)){
-                                                           echo "<tr>";
-                                                           echo "<td>" . $resultado['cod'] . "</td>"; 
-					                                                 echo "<td>" . $resultado['data_da_consulta'] . "</td>";
-                                                            echo "<td>" . $resultado['motivo'] . "</td>";
-                                                           echo "</tr>";
-																	 // <option value = "1"> ES </option>
-				                                                   }
+       while ($resultado = mysqli_fetch_array($consulta)) {
+        echo "<tbody>";
+        echo "<tr>";
+        echo "<td>" . $resultado['cod'] . "</td>"; 
+        echo "<td>" . $resultado['data_da_consulta'] . "</td>";
+        echo "<td>" . $resultado['motivo'] . "</td>";
+        echo "</tr>";
+        echo "</tbody>";
+        
+      }
+				
 			 ?>
-
-	   </table>
-	  </fieldset>
+      </table>
    </main>
   </div>
   </body>
--- php/mostrar2.php
+++ php/mostrar2.php
@@ -1,17 +1,21 @@
 <html>
   <head>
      <title>Cliente</title>
-	 <link rel = "stylesheet" href = "../estilo/estilizacao.css">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	   <link rel = "stylesheet" href = "../style/style.css">
   </head>
-  <body class = "cad_tutor">
+  <body class = "cad_tutor background-gradiente">
   <div id = "principal">
    <main>
 
 	  <fieldset>
-	   <legend>Consultar Dados</legend>
-      <div id="L" >
-	   <table border="1">
-
+    <div class="d-flex justify-content-center my-3"> 
+      <div>
+      <legend class="text-white">Dados Consultados</legend>
+      </div>
+    </div>
+      <div id="L">
+         <table class="table container">
 			 <?php
 				// importa dados de conexão
 			    include_once('conexao.php');
@@ -23,10 +27,12 @@
           
           $consulta = mysqli_query($conexao, "SELECT data_consulta, motivo_consulta motivo FROM agendar_consulta WHERE data_consulta = '$data' ORDER BY data_consulta");
   
-           echo "<tr>";
-           echo "<td>data da consulta</td>";
-           echo "<td>motivo da consulta</td>";
-           echo "</tr>";
+        echo "<thead>";
+          echo "<tr>"; 
+            echo '<th scope="col">Cod Consulta</th>';
+            echo '<th scope="col">Data da Consulta</th>';
+          echo "</tr>"; 
+       echo "</thead>"; 
   
   
                 
@@ -38,7 +44,6 @@
                                                              }
   
         ?>
-
 	   </table>
      </div>
 	  </fieldset>
